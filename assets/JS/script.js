f43299ec3e4aa01c32e3ea0745e39abb3fac6475
Thomas Dunlap <ttdunlap33@gmail.com>
2021-06-10 15:32:46 -0600

    repo links now deployed links

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,3 @@
-// var app = document.getElementById('title');
-
-// var typewriter = new Typewriter(app, {
-//   loop: true,
-//   delay: 75,
-// });
-
-// typewriter
-//   .pauseFor(2500)
-//   .typeString('Thomas Dunlap: a portfolio')
-//   .pauseFor(300)
-//   .deleteChars(10)
-//   .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-//   .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-//   .pauseFor(1000)
-//   .start();
-
 var app = document.getElementById('title');
 
 var customNodeCreator = function(character) {
